8d332f122c9d5dbae8d242bac87b2303ebca7f10
sujan khatri <sujansince2059@gmail.com>
2023-08-17 20:43:50 +0545

    list rendering

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,17 +1,75 @@
 import React from 'react';
 import PizzaData from "./PizzaData"
-
+const pizzaobj = [
+    {
+      name: "Focaccia",
+      ingredients: "Bread with italian olive oil and rosemary",
+      price: 6,
+      photoName: "pizzas/focaccia.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Margherita",
+      ingredients: "Tomato and mozarella",
+      price: 10,
+      photoName: "pizzas/margherita.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Spinaci",
+      ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+      price: 12,
+      photoName: "pizzas/spinaci.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Funghi",
+      ingredients: "Tomato, mozarella, mushrooms, and onion",
+      price: 12,
+      photoName: "pizzas/funghi.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Salamino",
+      ingredients: "Tomato, mozarella, and pepperoni",
+      price: 15,
+      photoName: "pizzas/salamino.jpg",
+      soldOut: true,
+    },
+    {
+      name: "Pizza Prosciutto",
+      ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+      price: 18,
+      photoName: "pizzas/prosciutto.jpg",
+      soldOut: false,
+    },
+  ];
 const Menu = () => {
-    return ( <main className='menu'>
-<h2>Our Menu</h2>
-    <PizzaData name="hello" image="./pizzas/funghi.jpg" ingredients="hel" price={20} />
-    <PizzaData />
-    <PizzaData />
+   
+    return (
+<main className='menu'>
+     <h2>Our Menu</h2>
+     
+      <ul className='pizzas'>
+        {
+            pizzaobj.map((pizza,index)=>
+
+                 (<PizzaData key={index} pizzadata={pizza} />   )
+            
+                )
+        }
+     
+    </ul> 
     
     
-    </main>
+</main>
     
     );
 }
  
 export default Menu;
+
+// console.log(pizzaobj.map((m)=>
+// {
+//     return m;
+// }))
--- src/PizzaData.jsx
+++ src/PizzaData.jsx
@@ -1,75 +1,32 @@
 import React, { cloneElement } from "react";
-const PizzaData = (props) => {
-    const pizzaData = [
-        {
-          name: "Focaccia",
-          ingredients: "Bread with italian olive oil and rosemary",
-          price: 6,
-          photoName: "pizzas/focaccia.jpg",
-          soldOut: false,
-        },
-        {
-          name: "Pizza Margherita",
-          ingredients: "Tomato and mozarella",
-          price: 10,
-          photoName: "pizzas/margherita.jpg",
-          soldOut: false,
-        },
-        {
-          name: "Pizza Spinaci",
-          ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
-          price: 12,
-          photoName: "pizzas/spinaci.jpg",
-          soldOut: false,
-        },
-        {
-          name: "Pizza Funghi",
-          ingredients: "Tomato, mozarella, mushrooms, and onion",
-          price: 12,
-          photoName: "pizzas/funghi.jpg",
-          soldOut: false,
-        },
-        {
-          name: "Pizza Salamino",
-          ingredients: "Tomato, mozarella, and pepperoni",
-          price: 15,
-          photoName: "pizzas/salamino.jpg",
-          soldOut: true,
-        },
-        {
-          name: "Pizza Prosciutto",
-          ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
-          price: 18,
-          photoName: "pizzas/prosciutto.jpg",
-          soldOut: false,
-        },
-      ];
+ const PizzaData = (props) => {
+   
       
       //data ends here
       // const pizzaName=
-      const pizzaName=pizzaData.map(pizname=>
-        {
-        return pizname.name
+      // const pizzaName=pizzaData.map(pizname=>
+      //   {
+      //   return pizname.name
         
-        }).map(nm=>
-          {
+      //   }).map(nm=>
+      //     {
              
-             return nm.indexOf(" ")>0? nm.slice(nm.indexOf(" ")):nm;
+      //        return nm.indexOf(" ")>0? nm.slice(nm.indexOf(" ")):nm;
              
             
-          })
+      //     })
       
     return ( 
-      <div className="pizza">
-        <img src={props.image} alt="" />
+      <li className="pizza">
+        <img src={props.pizzadata.photoName} alt="" />
         <div>
 
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price}</span>
+        <h3>{props.pizzadata.name}</h3>
+        <p>{props.pizzadata.ingredients}</p>
+        <span>{props.pizzadata.price}</span>
         </div>
-      </div>
+      </li>
      );
 }
- 
+//  export const a=4;
 export default PizzaData;
