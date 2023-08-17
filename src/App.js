f3471cb6b8240abb9a4651f91fefd1c992af68f9
sujan khatri <sujansince2059@gmail.com>
2023-08-17 09:57:00 +0545

    added components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react"
 import PizzaData from "./PizzaData"
+import Header from "./Header"
+import Menu from "./Menu"
+import Footer from "./Footer"
 
 function App() {
   return (
     <div className="App">
       <h1>hello</h1>
       <PizzaData />
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   )
 }
